d546eac74e211b8851517df2f7e5f9272b53b70a
bciereszynski <b.ciereszynski@wp.pl>
2024-10-01 22:30:38 +0200

    initial

---
+++ tutorials/app.py
@@ -0,0 +1,13 @@
+from tutorials import tutorial1 as t1, tutorial3 as t3, tutorial9 as t9, tutorial4 as t4, tutorial2 as t2, \
+    tutorial10 as t10, tutorial6 as t6, tutorial8 as t8, tutorial7 as t7, tutorial5 as t5
+
+t1.Tutorial1()
+t2.Tutorial2()
+t3.Tutorial3()
+t4.Tutorial4()
+t5.Tutorial5()
+t6.Tutorial6()
+t7.Tutorial7()
+t8.Tutorial8()
+t9.Tutorial9()
+t10.Tutorial10()
